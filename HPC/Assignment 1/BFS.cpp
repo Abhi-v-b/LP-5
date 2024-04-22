52a05d7fb9d9c4710e0468a6ff07aad72c41f0af
Abhi-v-b <134857248+Abhi-v-b@users.noreply.github.com>
2024-04-23 01:11:47 +0530

    Update BFS.cpp

--- HPC/Assignment 1/BFS.cpp
+++ HPC/Assignment 1/BFS.cpp
@@ -149,4 +149,76 @@ Execution time: 2.7e-05 seconds
 
 
 
+3rd Method:
 
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <ctime>
+#include <omp.h>
+using namespace std;
+
+int main() {
+    int num_vertices, num_edges, source;
+    cin >> num_vertices >> num_edges >> source;
+
+    vector<vector<int>> adj_list(num_vertices + 1);
+    for (int i = 0; i < num_edges; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    queue<int> q;
+    vector<bool> visited(num_vertices + 1, false);
+    q.push(source);
+    visited[source] = true;
+
+    double start = omp_get_wtime();
+
+    while (!q.empty()) {
+        int q_size = q.size();
+
+        #pragma omp parallel
+        {
+            #pragma omp for
+            for (int i = 0; i < q_size; i++) {
+                int curr_vertex;
+                #pragma omp critical
+                {
+                    curr_vertex = q.front();
+                    q.pop();
+                }
+                cout << curr_vertex << " ";
+
+                for (int j = 0; j < adj_list[curr_vertex].size(); j++) {
+                    int neighbour = adj_list[curr_vertex][j];
+                    #pragma omp critical
+                    {
+                        if (!visited[neighbour]) {
+                            visited[neighbour] = true;
+                            q.push(neighbour);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    double end = omp_get_wtime();
+    double duration = end - start;
+    cout << "\nExecution time: " << duration << " seconds" << endl;
+
+    return 0;
+}
+
+OutPut:
+4 4
+0
+0 1
+1 3
+0 2
+2 3
+0 2 1 3 
+Execution time: 0.000255912 seconds
